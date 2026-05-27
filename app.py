123e9bc2a207e1c537784d10dac2d7180d6a7b18
Caterin <fernandx.a.ch@gmail.com>
2026-05-27 18:27:23 -0500

    Actualizacion Bootstrap y MySQL

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ app = Flask(__name__)
 app.secret_key = "codemaster"
 
 # CONEXIÓN MYSQL
+
 conexion = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
+
+    host=os.getenv("DB_HOST", "localhost"),
+
+    port=int(os.getenv("DB_PORT", 3306)),
+
+    user=os.getenv("DB_USER", "root"),
+
+    password=os.getenv("DB_PASSWORD", ""),
+
+    database=os.getenv("DB_NAME", "codemaster"),
+
     autocommit=True
+
 )
 
 cursor = conexion.cursor(buffered=True)
